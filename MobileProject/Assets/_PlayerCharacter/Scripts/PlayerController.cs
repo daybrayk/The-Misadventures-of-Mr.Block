78b3aa598401489ae20d3239ae08f5e9f379df2a
daybrayk <braydonajohnson@gmail.com>
2019-06-26 11:02:28 -0400

    Reorganized the project folder so things are not all over the place

---
+++ MobileProject/Assets/_PlayerCharacter/Scripts/PlayerController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour {
+
+    #region Instance Variables
+    [Header("Instance Variables")]
+    [HideInInspector]public bool isMoving;
+    [HideInInspector]public bool canDash;
+    private float[] gravityTier;
+    private float swingForce;
+    private float dashForce;
+    #endregion
+
+    #region Object References
+    [Header("Object References")]
+    public GameUI gameUI;
+    private Rigidbody2D rb;
+    private RopeSystem rs;
+    private Animator m_anim;
+    #endregion
+
+    #region Particle Systems
+    [Header("Particle Systems")]
+    public ParticleSystem dashRight;
+    public ParticleSystem dashLeft;
+    public ParticleSystem squishPE;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        if (!m_anim)
+            m_anim = GetComponent<Animator>();
+        if (swingForce <= 0)
+            swingForce = 4.0f;
+        if (!rb)
+            rb = GetComponent<Rigidbody2D>();
+        rs = GetComponent<RopeSystem>();
+        if (dashForce <= 0)
+            dashForce = 6.0f;
+        if(gravityTier.Length == 0)
+            gravityTier = new float[3] { 2.5f, 5f, 10f };
+	}
+
+    private void Update()
+    {
+        m_anim.SetFloat("vVel", rb.velocity.y);
+        m_anim.SetFloat("hVel", rb.velocity.x);
+    }
+
+    #region Dynamic Gravity Update
+    private void FixedUpdate()
+    {
+        if(rb.velocity.y > 10.0f)
+            rb.velocity += Vector2.up * Physics2D.gravity.y * gravityTier[0] * Time.fixedDeltaTime;
+        else if(rb.velocity.y > 5.0f && rb.velocity.y < 10.0f)
+            rb.velocity += Vector2.up * Physics.gravity.y * gravityTier[0] * Time.fixedDeltaTime;
+    }
+    #endregion
+
+    public void Dash(int direction)
+    {
+        Vector2 dir = (rs.ropeHingeAnchor.transform.position - transform.position).normalized;
+        Vector2 perp = new Vector2(dir.y, -dir.x) * swingForce;
+        //rb.velocity = new Vector2(rb.velocity.x, 0);
+        switch(direction){
+            case 0:
+                dashRight.Play();
+                rb.AddRelativeForce(perp, ForceMode2D.Impulse);
+                break;
+            case 1:
+                dashRight.Play();
+                rb.AddRelativeForce(-perp, ForceMode2D.Impulse);
+                break;
+            case 2:
+                dashRight.Play();
+                rb.AddRelativeForce(transform.right * dashForce, ForceMode2D.Impulse);
+                break;
+            case 3:
+                dashLeft.Play();
+                rb.AddRelativeForce(-transform.right * dashForce, ForceMode2D.Impulse);
+                break;
+        }
+    }
+    #region Dynamic Gravity Update
+    public void Swing(Vector2 ropeDir)
+    {
+        Vector2 swingDir = new Vector2(ropeDir.y, -ropeDir.x).normalized;
+        rb.velocity = swingDir * rb.velocity.magnitude;
+    }
+    #endregion
+
+
+    public void Squish()
+    {
+        gameUI.RemoveSubscriber();
+        GameManager.instance.SaveGame();
+        squishPE.Play();
+        //GameManager.instance.LoadScene("TitleScene");
+    }
+
+    public void Slice()
+    {
+        gameUI.RemoveSubscriber();
+        GameManager.instance.SaveGame();
+        GameManager.instance.LoadScene("TitleScene 1");
+    }
+
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.tag == "KillZone" || c.tag == "Ground")
+        {
+            gameUI.RemoveSubscriber();
+            GameManager.instance.SaveGame();
+            GameManager.instance.LoadScene("TitleScene 1");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_anim.SetTrigger("collision");
+    }
+}
---
+++ MobileProject/Assets/_PlayerCharacter/Scripts/StateMachineBehaviours/IntroBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroBehaviour : StateMachineBehaviour {
+    public float distance;
+    public float time;
+    public float force;
+    private Transform m_player;
+    private Rigidbody2D m_rb;
+    private InputManager m_playerInput;
+    private bool m_runOnce;
+
+	  //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        if(!m_runOnce)
+        {
+            m_playerInput = animator.GetComponent<InputManager>();
+            m_player = animator.gameObject.transform;
+            m_rb = animator.gameObject.GetComponent<Rigidbody2D>();
+            m_rb.bodyType = RigidbodyType2D.Kinematic;
+            m_runOnce = true;
+        }
+        m_playerInput.enabled = false;
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        m_rb.MovePosition(new Vector2(m_player.position.x, m_player.position.y) + (Vector2.right * (distance / time) * Time.deltaTime));
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        m_rb.bodyType = RigidbodyType2D.Dynamic;
+        m_rb.AddForce(new Vector2(1.25f, 0.3f) * force, ForceMode2D.Impulse);
+        m_playerInput.enabled = true;
+        animator.Play("Player_Smile");
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
